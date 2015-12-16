c0e989f12865de2425e93338b7d5cf2874d9a90e
ckl <ckl13@mails.tsinghua.edu.cn>
2015-12-16 05:19:06 -0800

    data.py & model_v1.py

--- core/data/data.py
+++ core/data/data.py
@@ -1,10 +1,13 @@
 from json import loads, dumps
 import numpy as np
 import random
+import os
+
 class DataGenerator:
-    def __init__(self, dir_, dictionary):
+    def __init__(self, dir_, dictionary, img_path):
         with open(dir_, "r") as f:
             self.data = loads(f.read())
+        self.img_path = img_path
         self.data_index = 0
         self.word_dictionary_size = len(dictionary)
         self.dictionary = dictionary
@@ -35,8 +38,9 @@ class DataGenerator:
 
     def calcData(self):
         d = self.data[self.data_index]
-        sentence = d["sentence"][0]
-        image = np.load(d["image_feature"] + ".npy")
+        sentence = d['sentence'][0]
+        fpath = os.path.join(self.img_path, d['image_feature'].split('/')[-1])
+        image = np.load(fpath + ".npy")
 
         words = sentence.split(" ")
         # words = words[1:]
@@ -71,3 +75,50 @@ class DataGenerator:
     #     for index in range(0, batch_size):
     #         if self.hasNext():
     #             self.next()
+
+english_char = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+class DatasetSimple(DataGenerator):
+    def get_data_stream(self):
+        for idx in range(len(self.data)):
+            d = self.data[idx]
+            if len(d['sentence']) == 0:
+                continue
+            words = self.deal_sentence(d['sentence'][0])
+            if words == None:
+                continue
+            ipth = os.path.join(self.img_path, d['image_feature'].split('/')[-1]+'.npy')
+            if not os.path.exists(ipth):
+                continue
+            img = np.load(ipth)
+
+            _n = len(words)+1
+            data = np.zeros((1,1,self.word_dictionary_size, _n), dtype='float32')
+            label = np.zeros((1,1,self.word_dictionary_size, _n), dtype='float32')
+            img_ft = np.zeros((1,1,img.shape[0],_n), dtype='float32')
+
+
+            data[0,0,self.word_dictionary_size-2,0]=1
+            for ii in range(1, _n):
+                data[0,0,self.dictionary[words[ii-1]]-1, ii]=1
+
+            label[0,0,self.word_dictionary_size-1, _n-1]=1
+            for ii in range(0, _n-1):
+                label[0,0,self.dictionary[words[ii]]-1, ii]=1
+
+            for ii in range(0, _n):
+                img_ft[0,0,:,ii]=img
+
+            yield (data, label, img_ft)
+
+    def deal_sentence(self, stc):
+        words = []
+        for ii in stc.split(' '):
+            if len(ii)>0:
+                for ch in ii:
+                    if ch not in ii:
+                        return None
+                words.append(ii)
+#        if len(words) > 10:
+#            return None
+        return words
---
+++ layers/model_v1.py
@@ -0,0 +1,177 @@
+from layers import *
+import theano
+import theano.tensor as T
+import numpy as np
+from json import loads, dumps
+import sys
+import os
+sys.path.append("../core")
+from data.data import DataGenerator
+from data.data import DatasetSimple
+from time import localtime
+def printTime():
+    t = localtime()
+    print "time is %d:%d:%d"%(t[3], t[4], t[5])
+
+word_dictionary, data_generator = (None, None)
+
+def local_fprop(x, y_truth, image_feature): # x = sentences  y_truth is [[][]]
+    global word_dictionary
+
+    fc0 = ColumnWiseFullyConnected(
+            input_dim=len(word_dictionary),
+            output_dim=128,
+            nonlinearity=reluFunc,
+            name = "fc0"
+        )
+
+    fc1 = ColumnWiseFullyConnected(
+        input_dim=128,
+        output_dim=256,
+        nonlinearity=reluFunc,
+        name = "fc1"
+        )
+
+    rnn0 = LSTM(
+            input_dim=256,
+            output_dim=256,
+            # ,nonlinearity=reluFunc
+            name = "lstm0"
+            )
+
+    multimodal_rnn = ColumnWiseFullyConnected(
+            input_dim=256,
+            output_dim=256,
+            nonlinearity=reluFunc,
+            name = "multimodal_rnn"
+        )
+
+    multimodal_image = ColumnWiseFullyConnected(
+            input_dim=4096,
+            output_dim=256,
+            nonlinearity=reluFunc,
+            name = "multimodal_image"
+    )
+
+    multimodal_input = ColumnWiseFullyConnected(
+        input_dim=256,
+        output_dim=256,
+        nonlinearity=reluFunc,
+        name = "multimodal_input"
+    )
+
+    fc2 = ColumnWiseFullyConnected(
+        input_dim=256,
+        output_dim=len(word_dictionary),
+        nonlinearity=specialTanhFunc,
+        name = "fc2"
+        )
+
+    softmax = ColumnWiseSoftmax(
+        name = "softmax"
+    )
+
+    embeded1_out = fc0.fprop(x)
+    embeded2_out = fc1.fprop(embeded1_out)
+    rnn0_out = rnn0.fprop(embeded2_out)
+    # return rnn0_out ,rnn0_out , []
+    multimodal_rnn_out = multimodal_rnn.fprop(rnn0_out)
+    multimodal_image_out = multimodal_image.fprop(image_feature)
+    multimodal_input_out = multimodal_input.fprop(embeded2_out)
+    multimodal_out = multimodal_rnn_out + multimodal_input_out + multimodal_image_out
+    softmax_in = fc2.fprop(multimodal_out)
+    my_out = softmax.fprop(softmax_in)
+    # my_cost = softmax.cost(t3, y_truth)
+
+    def _local_cost(y, y_truth):
+        tmp = y * y_truth
+        # tmp = T.log(tmp.sum(axis=2)).mean(axis=2).mean()
+        tmp = T.log(tmp.max(axis=2)).sum(axis=2).mean()
+        tmp = -tmp
+        return tmp
+
+    my_cost = _local_cost(my_out, y_truth)
+
+    my_update = []
+    layers = [fc0, fc1, fc2, multimodal_rnn, multimodal_input, rnn0, softmax,
+        multimodal_image
+    ]
+    momemtum = 0.9
+    for layer in layers:
+        for p in layer.get_params():
+            lr = layer.learning_rate
+            wd = layer.weight_decay
+            g = T.grad(cost=my_cost, wrt=p)
+            p_delta = theano.shared(floatX(np.zeros(p.get_value().shape)))
+            my_update.append((p, p-lr*(g+wd*p)))
+            # my_update.append((p, p+p_delta*momemtum-lr*(g+wd*p)))
+            # my_update.append((p_delta, p_delta*momemtum-lr*(g+wd*p)))
+
+    return my_out, my_cost, my_update, layers
+
+
+def readDataFile(base_dir):
+    with open(os.path.join(base_dir, "word_dictionary.txt"), "r") as f:
+        word_dictionary = loads(f.read())
+    data_generator = DatasetSimple(os.path.join(base_dir, "config_all.conf"), word_dictionary, '/home/ckl/git/data/')
+    return word_dictionary, data_generator
+
+
+if __name__ == '__main__':
+    base_dir = "../../data"
+    word_dictionary, data_generator = readDataFile(base_dir)
+#    from IPython import embed;embed()
+    print "the size of dictionary is  " + str(len(word_dictionary))
+    data = T.tensor4()
+    label = T.tensor4()
+    image_feature = T.tensor4()
+
+    output, cost, update, layers = local_fprop(data, label, image_feature)
+    train_func = theano.function(inputs=[data, label, image_feature], outputs=[output, cost], updates=update,
+            on_unused_input='warn', allow_input_downcast=True)
+    test_func = theano.function(inputs=[data, label, image_feature], outputs=[output, cost],
+            on_unused_input='warn', allow_input_downcast=True)
+
+    # then start train with function
+    while True:
+        print ">> enter command"
+        print ">> "
+        command = raw_input()
+        if command == "t":
+            print ">> enter time"
+            t = int(raw_input())
+            print ">> enter words number"
+            cnt = int(raw_input())
+            while t > 0:
+                t -= 1
+                data_generator.resetData()
+                print "total data size is " + str(len(data_generator.data))
+                index = 0
+                for datapoint in data_generator.get_data_stream():
+                    index += 1
+                    print "train %s data" % (str(index))
+#                    train_data_sentence, train_data_result, train_data_image_feature = data_generator.calcData()
+                    train_data_sentence = datapoint[0]
+                    train_data_result = datapoint[1]
+                    train_data_image_feature = datapoint[2]
+                    output, cost = train_func(train_data_sentence, train_data_result, train_data_image_feature)
+                    print cost
+                    print data_generator.translate(output)
+                    print data_generator.translate(train_data_sentence)
+                    data_generator.next()
+                    if index == cnt:
+                        break
+        elif command == "shuffle":
+            data_generator.shuffle()
+        elif command == "save":
+            print ">> enter name of this data"
+            temp_name = raw_input()
+            for layer in layers:
+                layer.save_data(temp_name)
+        elif command == "load":
+            print ">> enter name of this data"
+            temp_name = raw_input()
+            for layer in layers:
+                layer.load_data(temp_name)
+        else:
+            print ">> error"
