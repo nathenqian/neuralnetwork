8951dd3d1d2bad960eeba0ad26315f9a9a1be560
nathenqian <zczyqdc@126.com>
2015-12-19 20:49:25 +0800

    data commit

--- core/data/data.py
+++ core/data/data.py
@@ -2,7 +2,8 @@ from json import loads, dumps
 import numpy as np
 import random
 import os
-
+import math
+import time
 class DataGenerator:
     def __init__(self, dir_, dictionary, img_path):
         with open(dir_, "r") as f:
@@ -188,3 +189,111 @@ class DataGeneratorFeature(DataGenerator):
         for i in range(0, batch_size):
             if self.hasNext():
                 self.next()
+
+
+class PoemDataGenerator:
+    def __init__(self, dir_, dictionary):
+        with open(dir_, "r") as f:
+            temp = loads(f.read())
+        self.rev_dictionary = {}
+        for i in dictionary:
+            self.rev_dictionary[dictionary[i]] = i
+        size = self.detectData(temp, self.rev_dictionary)
+        self.data = []
+        self.data_word_size = {i : 0 for i in range(len(dictionary))}
+        for poem in temp:
+            for i in range(3):
+                s = poem[i]
+                t = poem[i + 1]
+                p = self.calcSentenceProb(size, s) + self.calcSentenceProb(size, t)
+                print p
+                if p < 120000 * random.random():
+                    self.data.append((s, t))
+                    for z in s:
+                        self.data_word_size[z] += 1
+        calc_list = []
+        for i in self.data_word_size:
+            calc_list.append(self.data_word_size[i])
+        print max(calc_list)
+        time.sleep(3)
+        print calc_list
+        self.data_index = 0
+        self.word_dictionary_size = len(dictionary)
+        self.dictionary = dictionary
+
+    def calcSentenceProb(self, size, sentence):
+        ret = 0
+        for word in sentence:
+            cnt = size[word][1]
+            ret = ret + math.log(cnt) ** 5
+            # if cnt <= 100:
+            #     ret *= 100 / cnt
+            #     continue
+            # ret /= cnt / 100.0
+        return ret
+
+    def resetData(self):
+        self.data_index = 0
+
+    def shuffle(self):
+        random.shuffle(self.data)
+
+    def data(self):
+        return self.data[self.data_index]
+
+    def hasNext(self):
+        return self.data_index != len(self.data)
+
+    def next(self):
+        self.data_index += 1
+        return self.data[self.data_index - 1]
+
+    def calcData(self):
+        s = self.data[self.data_index][0]
+        t = self.data[self.data_index][1]
+
+        input_ = np.random.randn(1, 1, self.word_dictionary_size, 5) * 0
+        output = np.random.randn(1, 1, self.word_dictionary_size, 5) * 0
+
+        for index in range(0, 5):
+            input_[0, 0, s[index], index] = 1
+            output[0, 0, t[index], index] = 1
+
+        return (input_, output)
+
+    def list(self, cnt):
+        cnt = min(len(self.data), cnt)
+        if cnt == -1:
+            cnt = len(self.data)
+        ret = []
+        for i in range(0, cnt):
+            ret.append(self.data[i]["sentence"][0])
+        return ret
+
+    def showProb(self, output, correct):
+        ret = ""
+        ret_corr = ""
+        for i in range(output.shape[3]):
+            # print np.argmax(correct[0, 0, :, i], axis = 0)
+            # print np.argmax(output[0, 0, :, i], axis = 0)
+            # print np.max(output[0, 0, :, i], axis = 0), output[0, 0, np.argmax(correct[0, 0, :, i], axis = 0), i]
+            print output[0, 0, np.argmax(output[0, 0, :, i], axis = 0), i], output[0, 0, np.argmax(correct[0, 0, :, i], axis = 0), i]
+            n = np.argmax(output[0, 0, :, i], axis = 0)
+            ret = ret + " " + self.rev_dictionary[n]
+            ret_corr = ret_corr + " " + self.rev_dictionary[np.argmax(correct[0, 0, :, i], axis = 0)]
+        return ret + "\n" + ret_corr
+
+    def detectData(self, data, rev_dictionary):
+        size = []
+        for i in range(0, len(rev_dictionary)):
+            size.append((i, 0))
+        for poem in data:
+            for sentence in poem:
+                for word in sentence:
+                    size[word] = (word, size[word][1] + 1)
+        p = sorted(size, key = lambda a : -a[1])
+        with open("word_freq.txt", "w") as f:
+            for i in p:
+                f.write(str(i[1]) + " ")
+                f.write(rev_dictionary[i[0]].encode("utf8") + "\n")
+        return size
